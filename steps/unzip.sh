622c4c922b7a509de50557dd6da36ed7a1712abe
XIUWEN-ZHENG <96778918+XIUWEN-ZHENG@users.noreply.github.com>
2024-06-18 17:13:56 -0500

    Update unzip.sh

--- steps/unzip.sh
+++ steps/unzip.sh
@@ -1,20 +1,21 @@
 #!bin/sh
 download=$PWB/DatasetDownload
-database=data/raw
-mkdir -p data
-mkdir -p ${database}
+database=$PWB/data
+mkdir -p ${database}; mkdir -p ${database}/raw; mkdir -p ${database}/doc
 
 for file in `ls ${download} | grep "**.7z"`; do
+    echo "$file already merged"
+    if [grep "Json.7z"]; then
+        
+    fi
     7za x ${download}/${file} -r -o${database}
 done
 
 for file in `ls ${download} | grep "**.json"`; do
-    cp ${download}/${file} ${database}/${file}
+    cp ${download}/${file} ${database}/doc/${file}
 done
 
-for file in `ls ${database} | grep "**.7z"`; do
-    7za x ${database}/${file} -r -o${database}
-    rm ${database}/${file}
+for file in `ls ${database}/raw | grep "**.7z"`; do
+    7za x ${database}/raw/${file} -r -o${database}/raw
+    rm ${database}/raw/${file}
 done
-
-# ls -lR | grep "^d" | wc -l
