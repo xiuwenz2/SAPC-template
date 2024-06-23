08d8bf4b0d50428b863d581ab6d9f9307b7646b9
xiuwenz2 <96778918+xiuwenz2@users.noreply.github.com>
2024-06-22 20:52:15 -0500

    Add files via upload

--- steps/preprocess.sh
+++ steps/preprocess.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-stage=0
-stop_stage=2
+stage=1
+stop_stage=1
 
-usr=???
+usr=xiuwenz2
 source /home/${usr}/.bashrc
-PYTHON_VIRTUAL_ENVIRONMENT=/home/${usr}/.conda/envs/sapc
+PYTHON_VIRTUAL_ENVIRONMENT=/home/${usr}/.conda/envs/wav2vec
 conda activate ${PYTHON_VIRTUAL_ENVIRONMENT}
 
 cwd=$(pwd)
 release=2024-04-30
-splits="train dev test"
+# splits="train dev test"
+splits="dev test"
 
 ## run stage 0
 if [ $stage -le 0 ] && [ $stop_stage -ge 0 ]; then
@@ -32,8 +33,8 @@ if [ $stage -le 1 ] && [ $stop_stage -ge 1 ]; then
     echo "Stage 1: Generate .origin.tsv manifest..."
     mkdir -p ${cwd}/manifest
     for split in ${splits}; do
-        echo "writing ${split}.tsv to ${cwd}/manifest"
-        python ${cwd}/utils/generate_tsv.py \
+        echo "writing ${split}.tsv && ${split}.origin.wrd to ${cwd}/manifest"
+        python ${cwd}/utils/generate_manifest.py \
             --split ${split} \
             --data-dir ${cwd}/data/processed \
             --manifest-dir ${cwd}/manifest
@@ -44,10 +45,15 @@ fi
 if [ $stage -le 2 ] && [ $stop_stage -ge 2 ]; then
     echo "Stage 2: Generate .wrd label..."
     for split in ${splits}; do
-        echo "writing ${split}.origin.wrd to ${manifest_dir}"
-        python ${cwd}/utils/generate_wrd.py \
-                --tag ${split} \
-                --datadest ${datadest} \
-                --manifest-dir ${manifest_dir}/${manifest_tag}
+        echo "writing ${split}.origin.wrd && ${split}.tsv to ${cwd}/manifest"
+        python ${cwd}/utils/generate_origin_wrd.py \
+                --split ${split} \
+                --data-dir ${cwd}/data/processed \
+                --manifest-dir ${cwd}/manifest
+#         echo "writing ${split}.wrd to ${cwd}/manifest"
+#         python ${cwd}/utils/generate_wrd.py \
+#                 --tag ${split} \
+#                 --data-dir ${datadest} \
+#                 --manifest-dir ${cwd}/manifest/${split}
     done   
 fi
