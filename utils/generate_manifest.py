b09afd43d19feb426e3cdb042ba3ca6a935c500d
xiuwenz2 <96778918+xiuwenz2@users.noreply.github.com>
2024-06-22 21:10:32 -0500

    Update generate_manifest.py

--- utils/generate_manifest.py
+++ utils/generate_manifest.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# By xiuwenz2@illinois.edu, Oct.17, 2023.
+# By xiuwenz2@illinois.edu, June.22, 2024.
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
