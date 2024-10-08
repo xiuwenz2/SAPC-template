c166ffccb43deaf0c1cf05bcdf10c66b0a2857a6
xiuwenz2 <96778918+xiuwenz2@users.noreply.github.com>
2024-10-08 03:09:00 -0500

    Add files via upload

---
+++ template/inference.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# By xiuwenz2@illinois.edu, July 10, 2024.
+
+"""
+SAPC inference file with whisper base model. Please modify it w.r.t. your own model.
+"""
+
+import argparse, os
+from tqdm import tqdm
+import whisper
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data-pth", default="/taiga/data/processed", type=str, metavar="DATA_PTH", help="data_pth"
+    )
+    parser.add_argument(
+        "--manifest-pth", default="/taiga/manifest", type=str, metavar="MANIFEST_PTH", help="manifest_pth"
+    )
+    parser.add_argument(
+        "--split", default="test1", type=str, metavar="SPLIT", help="split"
+    )
+    parser.add_argument(
+        "--output-name", default="/taiga/downloads/???/???/inference/???.hypo", type=str, metavar="OUTPUT-NAME", help="???s represent team name, submission pk, and split."
+    )
+    return parser
+
+def main(args):
+    ### TO-DO: load model ###
+    model = whisper.load_model("base")
+    tokenizer = whisper.tokenizer.get_tokenizer(multilingual=False)
+    number_tokens = [
+        i
+        for i in range(tokenizer.eot)
+        if all(c in "0123456789$%&-–—+£=�*…•" for c in tokenizer.decode([i]).removeprefix(" "))
+    ]
+    
+    ### Dump Inference Results
+    manifest = os.path.join(args.manifest_pth, args.split + ".tsv")
+    with open(manifest, "r") as ftsv, open(args.output_name, "w") as fhypo:
+        next(ftsv)
+        for t in tqdm(ftsv.readlines()):
+            fname = t.strip().split()[0].split("/")[-1]
+            ### TO-DO: modify the following inference scripts ###
+            result = model.transcribe(
+                os.path.join(args.data_pth, args.split, fname),
+                suppress_tokens=[-1] + number_tokens,
+                temperature=0.0
+            )
+            print(result["text"].strip(), file=fhypo)
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+    main(args)
---
+++ template/run.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+stage=0
+stop_stage=3
+
+team_name=$1
+submission_pk=$2
+
+### TO-DO: specify your python version ###
+python_version=3.9
+source ~/.bashrc
+source ~/miniconda3/etc/profile.d/conda.sh
+PYTHON_ENVIRONMENT=${team_name}
+
+root=/taiga/downloads/${team_name}/${submission_pk}
+
+if [ $stage -le 0 ] && [ $stop_stage -ge 0 ]; then
+    echo "Stage 0: Install conda environment..."
+    if conda info --envs | grep -q ${PYTHON_ENVIRONMENT}; then
+        echo "Environment ${PYTHON_ENVIRONMENT} already exists. Activating it..."
+        conda activate ${PYTHON_ENVIRONMENT}
+    else
+        echo "Creating environment ${PYTHON_ENVIRONMENT}..."
+        conda create --name ${PYTHON_ENVIRONMENT} python=${python_version} --yes
+    fi
+fi
+conda activate ${PYTHON_ENVIRONMENT}
+
+if [ $stage -le 1 ] && [ $stop_stage -ge 1 ]; then
+    echo "Stage 1: Install dependencies..."
+    ### TO-DO: add any dependencies needed for model inference ###
+    pip install numpy==1.23.4
+    pip install torch==1.12.0+cu116 torchvision==0.13.0+cu116 torchaudio==0.12.0+cu116 -f https://download.pytorch.org/whl/cu116/torch_stable.html
+    pip install git+https://github.com/openai/whisper.git
+    pip install tqdm
+fi
+
+if [ $stage -le 2 ] && [ $stop_stage -ge 2 ]; then
+    echo "Stage 2: Running inference..."
+
+    splits='test1 test2'
+    output_pth=${root}/inference
+    mkdir -p ${output_pth}
+    len_test1=7601
+    len_test2=8043
+    ### TO-DO: change inference.py w.r.t. your model ###
+    for split in ${splits}; do
+        FILENAME=${output_pth}/${split}.hypo
+        LINE_COUNT=$(wc -l < "$FILENAME")
+        EXPECTED_LENGTH="len_${split}"
+        if [ "$LINE_COUNT" -eq "${!EXPECTED_LENGTH}" ]; then
+            echo "File already exists, skipping model inference..."
+        else
+            python ${root}/inference.py \
+                --split ${split} \
+                --output-name ${output_pth}/${split}.hypo
+        fi
+    done
+    conda deactivate
+fi
+
+if [ $stage -le 3 ] && [ $stop_stage -ge 3 ]; then
+    echo "Stage 3: Evaluating results..."
+
+    PYTHON_ENVIRONMENT=evaluate
+    conda activate ${PYTHON_ENVIRONMENT}
+
+    python3 /taiga/utils/evaluate.py \
+        --submission-team-name ${team_name} \
+        --submission-pk ${submission_pk}
+
+    conda deactivate
+fi
